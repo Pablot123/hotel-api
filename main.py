416172710d4286b528faa0013969feb7745ef497
Pablot123 <48389551+Pablot123@users.noreply.github.com>
2020-12-21 21:58:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ from fastapi import FastAPI
 from fastapi import HTTPException
 api = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
-origins = [
-"http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-"http://localhost", "http://localhost:8080", "https://hotel-app-g5.herokuapp.com/"
-]
+origins = [ "http://localhost.tiangolo.com", "https://localhost.tiangolo.com", "http://localhost", "http://localhost:8080", "https://hotel-app-g5.herokuapp.com"]
 api.add_middleware(
 CORSMiddleware, allow_origins=origins,
 allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
